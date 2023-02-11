4616f1bc2bf3e1acd2de182702956e07179486e8
Alexwell <935562189@qq.com>
2023-02-11 10:09:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,60 @@
-#gpu方式的mnist手写数字识别
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
-import torch.utils.data as Data
-import torchvision
-import matplotlib.pyplot as plt
+import sys
+sys.path.append('..')
+
 import numpy as np
+
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+from torchvision import models
+from torchvision import transforms as tfs
+from torchvision.datasets import ImageFolder
+
+# 定义数据预处理
+train_tf = tfs.Compose([
+    tfs.RandomResizedCrop(224),
+    tfs.RandomHorizontalFlip(),
+    tfs.ToTensor(),
+    tfs.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # 使用 ImageNet 的均值和方差
+])
+
+valid_tf = tfs.Compose([
+    tfs.Resize(256),
+    tfs.CenterCrop(224),
+    tfs.ToTensor(),
+    tfs.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# 使用 ImageFolder 定义数据集
+train_set = ImageFolder('./hymenoptera_data/train/', train_tf)
+valid_set = ImageFolder('./hymenoptera_data/val/', valid_tf)
+# 使用 DataLoader 定义迭代器
+train_data = DataLoader(train_set, 64, True, num_workers=4)
+valid_data = DataLoader(valid_set, 128, False, num_workers=4)
+
+# 使用预训练的模型
+net = models.resnet50(pretrained=True)
+
+# 将最后的全连接层改成二分类
+net.fc = nn.Linear(2048, 2)
+
+criterion = nn.CrossEntropyLoss()
+
 from adan import Adan
-# from LSAdam import LSAdam
-import time
- 
-EPOCH = 1
-BATCH_SIZE = 32
-LR = 0.001
-DOWNLOAD_MNIST = True
- 
-train_data = torchvision.datasets.MNIST(
-    root='./mnist',
-    train=True,
-    transform=torchvision.transforms.ToTensor(),
-    download=DOWNLOAD_MNIST
-)
-train_loader = Data.DataLoader(
-    dataset=train_data,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    num_workers=2
-)
-test_data = torchvision.datasets.MNIST(
-    root='./mnist',
-    train=False
-)
-#change in here
-test_x = Variable(torch.unsqueeze(test_data.test_data, dim=1)).type(torch.FloatTensor)[:2000].cuda()/255.   # Tensor on GPU
-test_y = test_data.test_labels[:2000].cuda()
- 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN,self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(1,16,5,1,2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16,32,5,1,2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
-        )
-        self.out = nn.Linear(32 * 7 * 7,10) #10分类的问题
- 
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = x.view(x.size(0),-1)
-        x = self.out(x)
-        return x
-        
-def main():
-    cnn = CNN()
-    cnn.cuda()
- 
-    # optimizer = optim.Adam(cnn.parameters(),lr=LR)
-    # optimizer = LSAdam(cnn.parameters())
-    optimizer = Adan(cnn.parameters(), foreach=False, fused=True)
-    loss_func = nn.CrossEntropyLoss()
-    
-    prof = torch.profiler.profile(
-        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
-        on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/CNN_fused_adan_single_tensor'),
-        record_shapes=True,
-        profile_memory=True,
-        with_stack=True
-    )
-    prof.start()
-    for epoch in range(EPOCH):
-        start_time = time.time()
-        for step,(x,y) in enumerate(train_loader):
-            b_x = Variable(x).cuda()
-            b_y = Variable(y).cuda()
-            output = cnn(b_x)
-            loss = loss_func(output,b_y)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            prof.step()
-            if step % 50 == 0:
-                test_output = cnn(test_x)
-    
-                # !!!!!!!! Change in here !!!!!!!!! #
-                pred_y = torch.max(test_output, 1)[1].cuda().data.squeeze()  # move the computation in GPU
-    
-                accuracy = torch.sum(pred_y == test_y).type(torch.FloatTensor) / test_y.size(0)
-                print('Epoch: ', epoch, '| train loss: %.4f' % loss.item(), '| test accuracy: %.2f' % accuracy)
-        end_time = time.time()
-        print("One Epoch cost ", end_time - start_time, " s.")
-    prof.stop()
-if __name__ == '__main__':
-    main()
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-3, fused=True)
+
+from utils import train
+print("Train with fused adam")
+train(net, train_data, valid_data, 20, optimizer, criterion)
+
+print("--------------------")
+print("Train with fused adan")
+betas = (0.98, 0.92, 0.9)
+optimizer = Adan(net.parameters(), betas=betas, lr=1e-3, foreach=True, fused=True)
+# 使用预训练的模型
+net = models.resnet50(pretrained=True)
+# 将最后的全连接层改成二分类
+net.fc = nn.Linear(2048, 2)
+train(net, train_data, valid_data, 20, optimizer, criterion)
